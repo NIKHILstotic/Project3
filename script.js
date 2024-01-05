0495b818c6d91b20a877f6b5b0fb2f1bfe418243
NIKHILstotic <139929422+NIKHILstotic@users.noreply.github.com>
2024-01-05 03:21:25 +0000

    updates

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 }); 
+//var a = document.querySelector("elem1")
+//var image = a.getAttribute("data-image")
+//console.log(image)
+
+var elemc = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemc.addEventListener("mouseenter",function(){
+fixed.style.display = "block"
+
+})
+elemc.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+    
+    })
+    var elem1 = document.querySelector("#elem1")
+    elem1.addEventListener("mouseenter",function(){
+      var image =  elem1.getAttribute("data-image")
+      fixed.style.backgroundImage =`url(${image})`
+    })
